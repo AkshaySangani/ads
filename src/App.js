58a3db11ce5073b64a7d17e7a9f2093d3ccb294c
AkshaySangani <akshaysangani2002@gmail.com>
2023-03-29 15:28:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import AdsComponent from './AdsComponent';
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <h1>Place To show Google AdSense</h1>
+            <AdsComponent dataAdSlot='9247698777' />
+        </>
+
+    );
+}
+
+export default App;
